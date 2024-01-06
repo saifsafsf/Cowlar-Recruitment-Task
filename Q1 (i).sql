29a0dc111ff08b5e874b0657fac58cc87dff49cf
Saif Ullah <sullah.bese20seecs@seecs.edu.pk>
2024-01-06 17:30:14 +0500

    cleaning

--- Q1 (i).sql
+++ Q1 (i).sql
@@ -1,4 +1,5 @@
-SELECT customer_id, SUM(quantity * unit_price) AS total_purchases
+SELECT customer_id, 
+       SUM(quantity * unit_price) AS total_purchases
 FROM customer_dataset
 GROUP BY customer_id
 ORDER BY total_purchases DESC
