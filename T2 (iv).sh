3a78af0722def650f3c048efd16a3a59fa1ebdad
Saif Ullah <sullah.bese20seecs@seecs.edu.pk>
2024-01-22 00:52:42 +0500

    Task 2 (iv) done

---
+++ T2 (iv).sh
@@ -0,0 +1,26 @@
+# NOTE: 
+# Replace "cowlar", "customers_dataset" with your db-name, collection-name respectively
+
+mongosh mongodb://localhost:27017/cowlar --eval '
+    db.customers_dataset.insertOne(
+        {
+            customer_id: "1108SU",
+            customer_name: "SAIF ULLAH",
+            orders: [
+                {
+                    order_id: 9999,
+                    order_date: new Date("2002-08-11"),
+                    shipment_date: new Date("2002-08-11"),
+                    products: [
+                        {
+                            product_id: "1108SAIF",
+                            product_description: "PRODUCT BY SAIF ULLAH",
+                            quantity: 11,
+                            unit_price: 1108
+                        }
+                    ]
+                }
+            ]
+        }
+    )
+';
