4848ac3d16a3882c1a41c4cdedb88fdf08ce1b9f
Saif Ullah <sullah.bese20seecs@seecs.edu.pk>
2024-01-25 20:14:24 +0500

    comments fixed

--- Task 1/T1 (i).sql
+++ Task 1/T1 (i).sql
@@ -1,5 +1,5 @@
 SELECT customer_id, 
-       SUM(quantity * unit_price) AS total_purchases  --purchases per product
+       SUM(quantity * unit_price) AS total_purchases  -- purchases per product
 FROM customer_dataset
 GROUP BY customer_id
 ORDER BY total_purchases DESC
--- Task 1/T1 (ii).sql
+++ Task 1/T1 (ii).sql
@@ -1,5 +1,5 @@
 SELECT product_id, 
-       SUM(unit_price * quantity) AS total_revenue  --revenue per product
+       SUM(unit_price * quantity) AS total_revenue  -- revenue per product
 FROM customer_dataset
 GROUP BY product_id
 ORDER BY total_revenue DESC;
